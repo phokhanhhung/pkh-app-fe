d8242f36d8f63d3ed58db79641d7ea0b4cc846d0
phokhanhhung <phokhanhhung2409@gmail.com>
2022-01-27 08:50:25 +0700

    aegr

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import LoginForm from './Components/LoginForm';
-import {HashRouter as Router, Routes, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import Chat from './Components/Chat';
 
